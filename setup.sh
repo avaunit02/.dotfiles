1e382c60785444077dd349e94f089fa0d1807a5b
Patrick Gordon <patrick@hadean.com>
2018-03-28 23:14:51 +0100

    stuff

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
-sudo pacman -Syu base base-devel aria2 bash bash-completion feh firefox git htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux
-
+sudo pacman -Syu base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux
 cd $HOME/.dotfiles
-
-stow bash gdb htop nvim ranger tmux weechat xdg
-
+stow */
 dconf load / < dconf-settings.ini
