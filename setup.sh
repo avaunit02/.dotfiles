1404c664a649b0072d23f4ba9c8f081d02b728d7
Patrick Gordon <patrick@hadean.com>
2018-03-29 16:42:14 +0100

    only update needed pkgs, use dirname instead of HOME

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux
-cd $HOME/.dotfiles
+sudo pacman -Syu --needed base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux
+cd $(dirname $0)
 stow */
 dconf load / < dconf-settings.ini
