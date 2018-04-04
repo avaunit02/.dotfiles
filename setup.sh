f37a09f33f12ff3ec6430e23b18b91dc11dc6973
Patrick Gordon <patrick@hadean.com>
2018-04-04 22:55:54 +0100

    dconf

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pacman -Syu --needed base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux
 cd $(dirname $0)
 stow */
+dconf reset -f /
 dconf load / < dconf.ini
