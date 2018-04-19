e4c0b5c7acd718ae4a136e8bd83b2e9b93877af4
Patrick Gordon <patrick@hadean.com>
2018-04-19 23:39:04 +0100

    update setup.sh for extension install instructions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-sudo pacman -Syu --needed base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux xsel
+sudo pacman -Syu --needed base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux xsel xorg-xprop
 cd $(dirname $0)
 stow */
 dconf reset -f /
 dconf load / < dconf.ini
+
+(cd gnome-extensions/.local/share/gnome-shell/extensions/hidetopbar@mathieu.bidon.ca/ && make schemas)
