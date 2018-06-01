1d7ae68a226617d8040e0f7afffe840f62b69026
Patrick Gordon <patrick@hadean.com>
2018-06-01 23:47:08 +0100

    revert gnome, re-add bspwm, sxhkd, xinit, alcritty

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-sudo pacman -Syu --needed base base-devel aria2 bash bash-completion feh firefox git gnome htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow tmux xsel xorg-xprop
+sudo pacman -Syu --needed base base-devel aria2 bash bash-completion bspwm feh firefox git htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow sxhkd tmux xsel xorg-xprop
 cd $(dirname $0)
 stow */
-dconf reset -f /
-dconf load / < dconf.ini
-
-(cd gnome-extensions/.local/share/gnome-shell/extensions/hidetopbar@mathieu.bidon.ca/ && make schemas)
