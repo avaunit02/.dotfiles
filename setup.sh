ecb34267ff9dfa0d3df8668b36aa94d30e2fb9c6
Patrick Gordon <patrick@hadean.com>
2018-06-03 18:44:38 +0100

    replace setup.sh with pkglist.txt

--- setup.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -Syu --needed base base-devel aria2 bash bash-completion bspwm feh firefox git htop imagemagick jq mlocate mpv neovim netpbm openssh pwgen ranger stow sxhkd tmux xsel xorg-xprop
-cd $(dirname $0)
-stow */
