45a02606f5f9737a3f0bf0e3210d88fb02e37b29
Ava Gordon <avagordon01@gmail.com>
2024-03-06 22:50:28 +0000

    delete old stuff

--- scripts/scripts/download.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-flexget
-aria2c --dir=/opt/download/ --input-file=/opt/download/list.txt --deferred-input true --seed-time=0
--- scripts/scripts/printer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-sudo systemctl start avahi-daemon.service
-sudo systemctl start cups.service
-$BROWSER localhost:631
